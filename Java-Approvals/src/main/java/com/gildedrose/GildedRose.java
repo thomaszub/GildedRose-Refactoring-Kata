2308bc5af05d377af28fe1ff81a25786754b95bb
Thomas Zub <thomas.zub@outlook.de>
2024-07-19 11:34:47 +0200

    Adjust sellIn earlier

--- Java-Approvals/src/main/java/com/gildedrose/GildedRose.java
+++ Java-Approvals/src/main/java/com/gildedrose/GildedRose.java
@@ -17,16 +17,19 @@ class GildedRose {
         if (item.name.equals("Sulfuras, Hand of Ragnaros")) {
             return;
         }
+
+        item.sellIn -= 1;
+
         if (item.name.equals("Aged Brie")
             || item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
             if (item.quality < 50) {
                 item.quality = item.quality + 1;
 
                 if (item.name.equals("Backstage passes to a TAFKAL80ETC concert") && item.quality < 50) {
-                    if (item.sellIn < 11) {
+                    if (item.sellIn < 10) {
                         item.quality = item.quality + 1;
                     }
-                    if (item.sellIn < 6) {
+                    if (item.sellIn < 5) {
                         item.quality = item.quality + 1;
                     }
                 }
@@ -34,8 +37,6 @@ class GildedRose {
         } else if (item.quality > 0) {
             item.quality = item.quality - 1;
         }
-
-        item.sellIn = item.sellIn - 1;
 
         if (item.sellIn < 0) {
             if (!item.name.equals("Aged Brie")) {
