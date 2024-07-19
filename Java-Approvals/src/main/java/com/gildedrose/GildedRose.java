7e776b17a6279f875856d53f3ee4e06ef4a79e90
Thomas Zub <thomas.zub@outlook.de>
2024-07-19 11:26:00 +0200

    Simplify if conditions

--- Java-Approvals/src/main/java/com/gildedrose/GildedRose.java
+++ Java-Approvals/src/main/java/com/gildedrose/GildedRose.java
@@ -19,18 +19,15 @@ class GildedRose {
             if (item.quality > 0 && !item.name.equals("Sulfuras, Hand of Ragnaros")) {
                 item.quality = item.quality - 1;
             }
-        } else {
-            if (item.quality < 50) {
-                item.quality = item.quality + 1;
+        } else if (item.quality < 50) {
+            item.quality = item.quality + 1;
 
-                if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                    if (item.sellIn < 11 && item.quality < 50) {
-                        item.quality = item.quality + 1;
-                    }
-
-                    if (item.sellIn < 6 && item.quality < 50) {
-                        item.quality = item.quality + 1;
-                    }
+            if (item.name.equals("Backstage passes to a TAFKAL80ETC concert") && item.quality < 50) {
+                if (item.sellIn < 11) {
+                    item.quality = item.quality + 1;
+                }
+                if (item.sellIn < 6) {
+                    item.quality = item.quality + 1;
                 }
             }
         }
