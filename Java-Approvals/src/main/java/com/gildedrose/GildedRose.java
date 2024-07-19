9854a8e5c0e98f95498c636a31ac926f3dc69505
Thomas Zub <thomas.zub@outlook.de>
2024-07-19 11:37:24 +0200

    Invert branches

--- Java-Approvals/src/main/java/com/gildedrose/GildedRose.java
+++ Java-Approvals/src/main/java/com/gildedrose/GildedRose.java
@@ -39,16 +39,16 @@ class GildedRose {
         }
 
         if (item.sellIn < 0) {
-            if (!item.name.equals("Aged Brie")) {
-                if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                    if (item.quality > 0) {
-                        item.quality = item.quality - 1;
-                    }
-                } else {
-                    item.quality = 0;
+            if (item.name.equals("Aged Brie")) {
+                if (item.quality < 50) {
+                    item.quality = item.quality + 1;
                 }
-            } else if (item.quality < 50) {
-                item.quality = item.quality + 1;
+            } else if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                item.quality = 0;
+            } else {
+                if (item.quality > 0) {
+                    item.quality = item.quality - 1;
+                }
             }
         }
     }
