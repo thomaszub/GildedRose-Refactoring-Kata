cafc1d28264f8c6c04f40f5e8541e8623d7a7633
Thomas Zub <thomas.zub@outlook.de>
2024-07-19 11:51:18 +0200

    Merge item related ifs

--- Java-Approvals/src/main/java/com/gildedrose/GildedRose.java
+++ Java-Approvals/src/main/java/com/gildedrose/GildedRose.java
@@ -24,12 +24,12 @@ class GildedRose {
             if (item.quality < 50) {
                 item.quality = item.quality + 1;
             }
-        }
-        if (item.name.equals("Aged Brie") && item.sellIn < 0) {
-            if (item.quality < 50) {
-                item.quality = item.quality + 1;
+            if (item.sellIn < 0) {
+                if (item.quality < 50) {
+                    item.quality = item.quality + 1;
+                }
+                return;
             }
-            return;
         }
 
         if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
@@ -45,10 +45,10 @@ class GildedRose {
                     }
                 }
             }
-        }
-        if (item.name.equals("Backstage passes to a TAFKAL80ETC concert") && item.sellIn < 0) {
-            item.quality = 0;
-            return;
+            if (item.sellIn < 0) {
+                item.quality = 0;
+                return;
+            }
         }
 
         if (!(item.name.equals("Aged Brie")
