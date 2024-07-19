367055d081b4014b26865f4a0f3e86449ded9e21
Thomas Zub <thomas.zub@outlook.de>
2024-07-19 14:00:58 +0200

    Remove foo test and add reference

--- Java-Approvals/src/test/java/com/gildedrose/GildedRoseApprovalTest.java
+++ Java-Approvals/src/test/java/com/gildedrose/GildedRoseApprovalTest.java
@@ -8,20 +8,9 @@ import org.approvaltests.Approvals;
 import org.approvaltests.reporters.DiffReporter;
 import org.approvaltests.reporters.UseReporter;
 import org.junit.jupiter.api.Test;
-import org.w3c.dom.Text;
 
 @UseReporter(DiffReporter.class)
 public class GildedRoseApprovalTest {
-
-	@Test
-	public void foo() {
-
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
-        GildedRose app = new GildedRose(items);
-        app.updateQuality();
-
-		Approvals.verifyAll("Items", items);
-	}
 
     @Test
     public void thirtyDays() {
