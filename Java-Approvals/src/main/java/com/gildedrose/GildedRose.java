2ed634f840594f39e3415b2d7bc196a97455d4e1
Thomas Zub <thomas.zub@outlook.de>
2024-07-19 11:28:06 +0200

    Invert if condition

--- Java-Approvals/src/main/java/com/gildedrose/GildedRose.java
+++ Java-Approvals/src/main/java/com/gildedrose/GildedRose.java
@@ -14,22 +14,22 @@ class GildedRose {
     }
 
     private static void updateQuality(Item item) {
-        if (!item.name.equals("Aged Brie")
-            && !item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-            if (item.quality > 0 && !item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                item.quality = item.quality - 1;
-            }
-        } else if (item.quality < 50) {
-            item.quality = item.quality + 1;
+        if (item.name.equals("Aged Brie")
+            || item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+            if (item.quality < 50) {
+                item.quality = item.quality + 1;
 
-            if (item.name.equals("Backstage passes to a TAFKAL80ETC concert") && item.quality < 50) {
-                if (item.sellIn < 11) {
-                    item.quality = item.quality + 1;
-                }
-                if (item.sellIn < 6) {
-                    item.quality = item.quality + 1;
+                if (item.name.equals("Backstage passes to a TAFKAL80ETC concert") && item.quality < 50) {
+                    if (item.sellIn < 11) {
+                        item.quality = item.quality + 1;
+                    }
+                    if (item.sellIn < 6) {
+                        item.quality = item.quality + 1;
+                    }
                 }
             }
+        } else if (item.quality > 0 && !item.name.equals("Sulfuras, Hand of Ragnaros")) {
+            item.quality = item.quality - 1;
         }
 
         if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
