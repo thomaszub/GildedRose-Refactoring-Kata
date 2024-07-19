9771748f407caa66a52a6cb3cc8731c74bd49583
Thomas Zub <thomas.zub@outlook.de>
2024-07-19 11:32:25 +0200

    Early return if item is Sulfuras

--- Java-Approvals/src/main/java/com/gildedrose/GildedRose.java
+++ Java-Approvals/src/main/java/com/gildedrose/GildedRose.java
@@ -14,6 +14,9 @@ class GildedRose {
     }
 
     private static void updateQuality(Item item) {
+        if (item.name.equals("Sulfuras, Hand of Ragnaros")) {
+            return;
+        }
         if (item.name.equals("Aged Brie")
             || item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
             if (item.quality < 50) {
@@ -28,18 +31,16 @@ class GildedRose {
                     }
                 }
             }
-        } else if (item.quality > 0 && !item.name.equals("Sulfuras, Hand of Ragnaros")) {
+        } else if (item.quality > 0) {
             item.quality = item.quality - 1;
         }
 
-        if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-            item.sellIn = item.sellIn - 1;
-        }
+        item.sellIn = item.sellIn - 1;
 
         if (item.sellIn < 0) {
             if (!item.name.equals("Aged Brie")) {
                 if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                    if (item.quality > 0 && !item.name.equals("Sulfuras, Hand of Ragnaros")) {
+                    if (item.quality > 0) {
                         item.quality = item.quality - 1;
                     }
                 } else {
@@ -48,7 +49,6 @@ class GildedRose {
             } else if (item.quality < 50) {
                 item.quality = item.quality + 1;
             }
-
         }
     }
 
