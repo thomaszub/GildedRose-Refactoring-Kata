03ca0b1608eda98b69988d963d8cfff050bcc929
Thomas Zub <thomas.zub@outlook.de>
2024-07-19 12:13:39 +0200

    Limit quality at end for other items

--- Java-Approvals/src/main/java/com/gildedrose/GildedRose.java
+++ Java-Approvals/src/main/java/com/gildedrose/GildedRose.java
@@ -30,12 +30,12 @@ class GildedRose {
 
     private static void updateOther(Item item) {
         item.sellIn -= 1;
-        if (item.quality > 0) {
+        item.quality -= 1;
+        if (item.sellIn < 0) {
             item.quality -= 1;
         }
-
-        if (item.quality > 0 && item.sellIn < 0) {
-            item.quality -= 1;
+        if (item.quality < 0) {
+            item.quality = 0;
         }
     }
 
