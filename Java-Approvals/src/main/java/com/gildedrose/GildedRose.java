859df716a7dfa9e678d851eed3c0d43fccb8f7aa
Thomas Zub <thomas.zub@outlook.de>
2024-07-19 11:42:45 +0200

    Remove nested branches

--- Java-Approvals/src/main/java/com/gildedrose/GildedRose.java
+++ Java-Approvals/src/main/java/com/gildedrose/GildedRose.java
@@ -38,18 +38,18 @@ class GildedRose {
             item.quality = item.quality - 1;
         }
 
-        if (item.sellIn < 0) {
-            if (item.name.equals("Aged Brie")) {
-                if (item.quality < 50) {
-                    item.quality = item.quality + 1;
-                }
-            } else if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                item.quality = 0;
-            } else {
-                if (item.quality > 0) {
-                    item.quality = item.quality - 1;
-                }
+        if (item.name.equals("Aged Brie") && item.sellIn < 0) {
+            if (item.quality < 50) {
+                item.quality = item.quality + 1;
             }
+            return;
+        }
+        if (item.name.equals("Backstage passes to a TAFKAL80ETC concert") && item.sellIn < 0) {
+            item.quality = 0;
+            return;
+        }
+        if (item.quality > 0 && item.sellIn < 0) {
+            item.quality = item.quality - 1;
         }
     }
 
